f827040affd2373390293e9a0cb1e4d5c17806b0
Константин Бровцев <brovan@yandex.ru>
2023-11-30 04:52:18 +0200

    fix: change deploy uri to https

--- packages/client/src/const/api.ts
+++ packages/client/src/const/api.ts
@@ -2,7 +2,7 @@ export const baseUrl = 'https://ya-praktikum.tech/api/v2';
 export const staticBaseUrl = 'https://ya-praktikum.tech/api/v2/resources';
 
 const releaseUri = 'https://alchemists-tanks-30.ya-praktikum.tech';
-const localServerBaseUrl = __NODE_ENV__ === 'development' ? 'localhost' : releaseUri;
+const localServerBaseUrl = __NODE_ENV__ === 'development' ? 'http://localhost' : releaseUri;
 const serverPort = __NODE_ENV__ === 'development' ? __SERVER_PORT__ : 80;
-export const localServerApi = `http://${localServerBaseUrl}:${serverPort}/api`;
-export const localServerYandexApi = `http://${localServerBaseUrl}:${serverPort}/api/v2`;
+export const localServerApi = `${localServerBaseUrl}:${serverPort}/api`;
+export const localServerYandexApi = `${localServerBaseUrl}:${serverPort}/api/v2`;
