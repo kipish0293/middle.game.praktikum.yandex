4d7133275e0485096d82ec8798ebe7fc2a048d94
Константин Бровцев <brovan@yandex.ru>
2023-11-30 05:06:04 +0200

    fix: change port for https (#65)

--- packages/client/src/const/api.ts
+++ packages/client/src/const/api.ts
@@ -3,6 +3,6 @@ export const staticBaseUrl = 'https://ya-praktikum.tech/api/v2/resources';
 
 const releaseUri = 'https://alchemists-tanks-30.ya-praktikum.tech';
 const localServerBaseUrl = __NODE_ENV__ === 'development' ? 'http://localhost' : releaseUri;
-const serverPort = __NODE_ENV__ === 'development' ? __SERVER_PORT__ : 80;
+const serverPort = __NODE_ENV__ === 'development' ? `${__SERVER_PORT__}` : '';
 export const localServerApi = `${localServerBaseUrl}:${serverPort}/api`;
 export const localServerYandexApi = `${localServerBaseUrl}:${serverPort}/api/v2`;
