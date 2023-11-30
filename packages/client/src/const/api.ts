fd6fa82acf931ea1925c5339f6a68f12cb19ee47
Константин Бровцев <brovan@yandex.ru>
2023-11-30 04:08:22 +0200

    fix: change release uri

--- packages/client/src/const/api.ts
+++ packages/client/src/const/api.ts
@@ -1,7 +1,8 @@
 export const baseUrl = 'https://ya-praktikum.tech/api/v2';
 export const staticBaseUrl = 'https://ya-praktikum.tech/api/v2/resources';
 
-const localServerBaseUrl = __NODE_ENV__ === 'development' ? 'localhost' : '51.250.103.103';
+const releaseUri = 'https://alchemists-tanks-30.ya-praktikum.tech';
+const localServerBaseUrl = __NODE_ENV__ === 'development' ? 'localhost' : releaseUri;
 const serverPort = __NODE_ENV__ === 'development' ? __SERVER_PORT__ : 80;
 export const localServerApi = `http://${localServerBaseUrl}:${serverPort}/api`;
 export const localServerYandexApi = `http://${localServerBaseUrl}:${serverPort}/api/v2`;
